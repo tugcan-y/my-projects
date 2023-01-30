86eee1db5eafed4852b2e20630bfba00e31b9167
tugcan-y <tugcanyildiz.de@gmail.com>
2023-01-30 12:13:05 +0300

    The project has been changed

--- aws/projects/Project-001-Roman-Numerals-Converter/CLI_solution/userdata.sh
+++ aws/projects/Project-001-Roman-Numerals-Converter/CLI_solution/userdata.sh
@@ -2,9 +2,8 @@
 yum update -y
 yum install python3 -y
 pip3 install flask
-yum install git -y
 cd /home/ec2-user
-wget -P templates https://raw.githubusercontent.com/serdarcw/cli_deneme/master/templates/index.html
-wget -P templates https://raw.githubusercontent.com/serdarcw/cli_deneme/master/templates/result.html
-wget https://raw.githubusercontent.com/serdarcw/cli_deneme/master/app.py
+wget -P templates https://raw.githubusercontent.com/tugcan-y/my-projects/main/aws/projects/Project-001-Roman-Numerals-Converter/templates/index.html
+wget -P templates https://raw.githubusercontent.com/tugcan-y/my-projects/main/aws/projects/Project-001-Roman-Numerals-Converter/templates/result.html
+wget https://raw.githubusercontent.com/tugcan-y/my-projects/main/aws/projects/Project-001-Roman-Numerals-Converter/app.py
 python3 app.py
